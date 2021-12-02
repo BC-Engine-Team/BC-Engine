e3b1d6d11318e54ddc0aff682bc8a7c16efcfcf2
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-01 22:43:04 -0500

    Finished the navBar

--- client/src/components/NavB.js
+++ client/src/components/NavB.js
@@ -3,37 +3,63 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import { LinkContainer } from "react-router-bootstrap"
 import logo from '../Images/logo.png'
+import { useState } from 'react'
 
-const NavB = () => {
-    return (
-        <Navbar variant="dark" bg="dark" expand="lg" collapseOnSelect className="mb-2">
-            <Container fluid>
-                <LinkContainer to="/dashboard">
+const NavB = (props) => {
+    const [state, setState] = useState(props)
+    var username = "User";
+
+    if(state.page === "login") {
+        return (
+            <Navbar variant="dark" bg="dark" className="mb-2">
+                <Container fluid className="justify-content-center">
                     <Navbar.Brand>
                         <img alt="logo" src={logo} width="30" height="30" className="d-inline-block align-top" />
                         {' '} B&C Engine
                     </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <LinkContainer to="/dashboard">
-                            <Nav.Link>Dashboard</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/reports">
-                            <Nav.Link>Reports</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/users">
-                            <Nav.Link>Users</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/manage">
-                            <Nav.Link>Manage</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+                </Container>
+            </Navbar>
+        )
+    }
+    else {
+        return (
+            <Navbar variant="dark" bg="dark" expand="md" collapseOnSelect className="mb-2">
+                <Container fluid>
+                    <LinkContainer to="/dashboard">
+                        <Navbar.Brand>
+                            <img alt="logo" src={logo} width="30" height="30" className="d-inline-block align-top" />
+                            {' '} B&C Engine
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <LinkContainer to="/dashboard" className="px-2">
+                                <Nav.Link>Dashboard</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/reports" className="px-2">
+                                <Nav.Link>Reports</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/users" className="px-2">
+                                <Nav.Link>Users</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/manage" className="px-2">
+                                <Nav.Link>Manage</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                        <Nav className="justify-content-end">
+                            <Navbar.Text className="me-3">
+                                Signed in as: {username}
+                            </Navbar.Text>
+                            <LinkContainer to="/login">
+                                <Nav.Link>Sign out</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
 }
 
 export default NavB
