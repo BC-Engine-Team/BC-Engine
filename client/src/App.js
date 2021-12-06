9de564d2c61c481bf3acb4b5a833ff46967b75b6
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-05 22:15:27 -0500

    Revert "Merge branch 'main' into conf/BCE-43/Data_access_layer"
    This reverts commit 2a76991b44b3432448f438869ad2c23c22b5bd01, reversing
    changes made to 521503dd882ce3104f48b68576378595614bac4a.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
+import logo from "./banana_PNG835.png";
 import "./App.css";
-import Dashboard from "./pages/Dashboard";
-import Reports from "./pages/Reports";
-import Users from "./pages/Users";
-import Manage from "./pages/Manage";
-import Login from "./pages/Login";
-import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
-    <div>
-      {/* Routes to pages */}
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Navigate replace to="login" />}/>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route exact path="/reports" element={<Reports />} />
-          <Route exact path="/users" element={<Users />} />
-          <Route exact path="/manage" element={<Manage />} />
-        </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "B&C Engine loading..." : data}</p>
+      </header>
     </div>
   );
 }
