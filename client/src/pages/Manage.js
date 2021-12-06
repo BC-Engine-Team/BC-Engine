56853ed2406e1109116b41ae89f3d272e8475f4c
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-05 22:20:23 -0500

    Revert "Revert "Merge branch 'main' into conf/BCE-43/Data_access_layer""
    This reverts commit 9de564d2c61c481bf3acb4b5a833ff46967b75b6.

---
+++ client/src/pages/Manage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavB from '../components/NavB'
+import UnderConstruction from '../components/UnderConstruction'
+
+const Manage = () => {
+    return (
+        <div>
+            <NavB />
+            <UnderConstruction pageName="Manage"/>
+        </div>
+    )
+}
+
+export default Manage
