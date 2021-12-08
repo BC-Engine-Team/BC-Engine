79d7bd157904879ab27fa6d280cc45ffb5e5dae4
cjayneb <70963776+cjayneb@users.noreply.github.com>
2021-12-07 23:21:14 -0500

    Conf(BCE-33): Integrate AUTH and DAL (#7)
    * Modified structure, integrated getting access token and authenticate access token
    
    * Integrated logout and refresh token
    
    * Fixed small issues

---
+++ server/routes/user.routes.js
@@ -0,0 +1,23 @@
+const users = require("../controllers/user.controller");
+let router = require("express").Router();
+const authService = require('../services/auth.service');
+
+
+// Create new User
+router.post("/", users.create);
+
+// Fetch all users with authentication
+router.get("/", authService.authenticateToken, users.findAll);
+
+// Authenticate user
+router.post("/authenticate", users.authenticateUserWithEmail);
+
+// Fetch admins
+router.get("/admins", users.getAdmins);
+
+// Refresh JWT
+router.post("/refresh", authService.refreshToken);
+
+router.delete("/logout", authService.logout);
+
+module.exports = router;
---
+++ server/services/auth.service.js
@@ -0,0 +1,64 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// should reference variables in .env
+const ACCESS_TOKEN_SECRET="773ab1b4d3e80db2d7ab5b227e28d4007985017a165fad9bd602692d8302ee23a9b8f8d2d29b65c375a9d8673a8bada595a5c72c776352fb9eac74c756680db9";
+const REFRESH_TOKEN_SECRET="c208182c162568cb40fba6ee43762c47d2b9d7689b4c2dfec7020213f79e8a855e6f1d2aeb2d65da21b5c564e7950d4ea6cfb579416ccf570c9ee87dc5c119ea";
+
+let refreshTokens = []
+
+exports.getTokens = (user) => {
+    //Authenticate the user
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, REFRESH_TOKEN_SECRET);
+    console.log(accessToken)
+  
+    //it acts as the database for the token, of course it is going to be different
+    refreshTokens.push(refreshToken);
+  
+    return [accessToken, refreshToken];
+};
+
+exports.authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+ 
+    if(token == null){
+      return res.sendStatus(401);
+    }
+ 
+    jwt.verify(token, ACCESS_TOKEN_SECRET, (err) => {
+      if(err){
+        return res.sendStatus(403);
+      }
+      next();
+    });
+};
+
+exports.refreshToken = (req, res) => {
+    const refreshToken = req.body.token; 
+    if(refreshToken == null){
+        return res.sendStatus(401);
+    }
+    if(!refreshTokens.includes(refreshToken)){
+        return res.sendStatus(403);
+    }
+    jwt.verify(refreshToken, REFRESH_TOKEN_SECRET, (err, user) => {
+        if(err){
+          res.sendStatus(403);
+        }
+        const accessToken = generateAccessToken({ email: user.email });
+        res.json({ accessToken: accessToken });
+    })
+};
+
+exports.logout = (req, res) => {
+    if(!req.body.token) res.sendStatus(400);
+    refreshTokens = refreshTokens.filter(t => t != req.body.token);
+    res.sendStatus(204);
+};
+
+//function to generate the access token
+function generateAccessToken(user){
+  return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: '15m'} );
+}
