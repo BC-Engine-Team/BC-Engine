fad4957108df568d13b003e85febc2a8f5691334
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-08 06:39:02 -0500

    Added header to handle logout (CORS)

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, POST, GET');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
