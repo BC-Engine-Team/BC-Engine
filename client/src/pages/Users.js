a68d7e1afb8dc66f8a850049fb297e204eb0b932
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-15 16:24:34 -0500

    moved css and added form animation for add and modify user

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -4,8 +4,9 @@ import Cookies from 'universal-cookie'
 import NavB from '../components/NavB'
 import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
-import '../DeleteButton.scss'
-import '../EditButton.scss'
+import CloseButton from 'react-bootstrap/CloseButton'
+import '../styles/DeleteButton.scss'
+import '../styles/EditButton.scss'
 import Icon from '@mdi/react'
 import { mdiDeleteEmpty } from '@mdi/js';
 import { mdiDelete } from '@mdi/js';
@@ -18,20 +19,45 @@ const Users = () => {
     let navigate = useNavigate();
     const cookies = new Cookies();
 
-    const [users, setUsers] = useState([{name: "", email: "", role: ""}]);
+    const [formEnabled, setFormEnabled] = useState({
+        table: "container", 
+        form: "d-none",
+    });
+
+    const [users, setUsers] = useState([
+        {name: "", email: "", role: ""}
+    ]);
+
+    const enableForm = () => {
+        setFormEnabled({
+            table: "container-form-enabled-table",
+            form: "container-form-enabled-form",
+        });
+    }
+
+    const disableForm = () => {
+        setFormEnabled({
+            table: "container",
+            form: "d-none",
+        });
+    }
 
     const handleAddUser = () => {
+        console.log("Add user");
+        enableForm();
     }
 
     const handleEditUser = (email) => {
-       
+        console.log("Edit user with email: " + email);
+        enableForm();
+
     }
 
     const handleDeleteUser = (email) => {
-        
+        console.log("Delete user with email: " + email);
     }
 
-    const handleRefresh = () => {
+     const handleRefresh = () => {
         let header = {
             'authorization': "Bearer " + cookies.get("accessToken")
         }
@@ -41,7 +67,7 @@ const Users = () => {
         Axios.get("http://localhost:3001/users/", {headers: header})
         .then((response) => {
             console.log(response.data);
-            setUsers(response.data)
+            setUsers(response.data);
         })
         .catch((error) => {
             if(error.response) {
@@ -75,85 +101,92 @@ const Users = () => {
     return (
         <div>
             <NavB />
-            <div className="container">
-                <div className="card shadow m-5">
-                    <Table responsive="xl">
-                        <thead className='bg-light'>
-                            <tr key="0">
+            <div className="justify-content-center mainContainer">
+                <div className={formEnabled.table}>
+                    <div className="card shadow m-5 uTable">
+                        <Table responsive="xl" hover>
+                            <thead className='bg-light'>
+                                <tr key="0">
 
-                                <th>
-                                    <div className="justify-content-center d-flex">
-                                        #
-                                    </div>
-                                </th>
-                                <th>NAME</th>
-                                <th>EMAIL</th>
-                                <th>ROLE</th>
-                                <th>
-                                    <div className="d-flex justify-content-center">
-                                        <Button 
-                                            className="btn py-0 shadow-sm border " 
-                                            onClick={handleAddUser}>
-                                            Add User
-                                        </Button>
-                                    </div>
-                                </th>
+                                    <th>
+                                        <div className="justify-content-center d-flex">
+                                            #
+                                        </div>
+                                    </th>
+                                    <th>NAME</th>
+                                    <th>EMAIL</th>
+                                    <th>ROLE</th>
+                                    <th>
+                                        <div className="d-flex justify-content-center">
+                                            <Button 
+                                                className="btn py-0 shadow-sm border " 
+                                                onClick={handleAddUser}>
+                                                Add User
+                                            </Button>
+                                        </div>
+                                    </th>
 
-                            </tr>
-                        </thead>
+                                </tr>
+                            </thead>
 
-                        <tbody>
-                            {users.map (u => {
-                                counter++;
-                                return (
-                                    <tr key={counter}>
+                            <tbody>
+                                {users.map (u => {
+                                    counter++;
+                                    return (
+                                        <tr key={counter}>
 
-                                        <td>
-                                            <div className="justify-content-center d-flex">
-                                                {counter}
-                                            </div>
-                                        </td>
-                                        <td>{u.name}</td>
-                                        <td>{u.email}</td>
-                                        <td>{u.role}</td>
+                                            <td>
+                                                <div className="justify-content-center d-flex">
+                                                    {counter}
+                                                </div>
+                                            </td>
+                                            <td>{u.name}</td>
+                                            <td>{u.email}</td>
+                                            <td>{u.role}</td>
 
-                                        <td className="py-1">
-                                            <div className="d-flex justify-content-center">
-                                                <button className="btnEdit btn-edit" onClick={handleEditUser(u.email)}>
-                                                    <Icon path={mdiPencil} 
-                                                        className="mdi mdi-edit" 
-                                                        title="edit Button"
-                                                        size={1}
-                                                        horizontal/>
-                            
-                                                    <Icon path={mdiPencilOutline}
-                                                        className="mdi mdi-edit-empty" 
-                                                        title="edit Button empty"
-                                                        size={1}
-                                                        horizontal/>
-                                                </button>
+                                            <td className="py-1">
+                                                <div className="d-flex justify-content-center">
+                                                    <button className="btnEdit btn-edit" onClick={() => handleEditUser(u.email)}>
+                                                        <Icon path={mdiPencil} 
+                                                            className="mdi mdi-edit" 
+                                                            title="edit Button"
+                                                            size={1}
+                                                            horizontal/>
+                                
+                                                        <Icon path={mdiPencilOutline}
+                                                            className="mdi mdi-edit-empty" 
+                                                            title="edit Button empty"
+                                                            size={1}
+                                                            horizontal/>
+                                                    </button>
 
-                                                <button className="btnDelete btn-delete" onClick={handleDeleteUser(u.email)}>
-                                                    <Icon path={mdiDelete} 
-                                                        className="mdi mdi-delete" 
-                                                        title="delete Button"
-                                                        size={1}
-                                                        horizontal/>
-                            
-                                                    <Icon path={mdiDeleteEmpty}
-                                                        className="mdi mdi-delete-empty" 
-                                                        title="delete Button empty"
-                                                        size={1}
-                                                        horizontal/>
-                                                </button>
-                                            </div>
-                                        </td>
+                                                    <button className="btnDelete btn-delete" onClick={() => handleDeleteUser(u.email)}>
+                                                        <Icon path={mdiDelete} 
+                                                            className="mdi mdi-delete" 
+                                                            title="delete Button"
+                                                            size={1}
+                                                            horizontal/>
+                                
+                                                        <Icon path={mdiDeleteEmpty}
+                                                            className="mdi mdi-delete-empty" 
+                                                            title="delete Button empty"
+                                                            size={1}
+                                                            horizontal/>
+                                                    </button>
+                                                </div>
+                                            </td>
 
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </Table>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+                <div className={formEnabled.form}>
+                    <div className="card shadow m-5 uForm">
+                        <CloseButton onClick={disableForm}/>
+                    </div>
                 </div>
             </div>
         </div>
