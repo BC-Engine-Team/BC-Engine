fcc60ce30dc3b41fa89c48f39c2d808ffa321900
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-15 13:55:34 -0500

    Conf(BCE-73):Enhance_JWT_security_On_The_Front-end (#11)
    * Added cookies
    
    * Cookies now working and deleting themselves on logout
    
    * Handled roles even if user tries to enter a url directly
    
    * modifies login.js
    
    * remove cookies after request

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Cookies from 'universal-cookie'
 import NavB from '../components/NavB'
 import UnderConstruction from '../components/UnderConstruction'
 
 const Users = () => {
+
+    let navigate = useNavigate();
+    const cookies = new Cookies();
+
+    useEffect(() => {
+        if (cookies.get("accessToken") === undefined) {
+            navigate("/login");
+        }
+        else if(cookies.get("role") !== "admin") {
+            navigate("/dashboard");
+        } 
+    });
+
     return (
         <div>
             <NavB />
