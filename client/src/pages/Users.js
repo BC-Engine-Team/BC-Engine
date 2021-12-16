9c8a9b06c57fa17306937f26018ec0088a52bc7f
cjayneb <70963776+cjayneb@users.noreply.github.com>
2021-12-15 21:13:44 -0500

    Started add user form

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -15,6 +15,10 @@ import { mdiPencil } from '@mdi/js';
 import { mdiPencilOutline } from '@mdi/js';
 import Axios from 'axios';
 
+import Form from 'react-bootstrap/Form'
+import FloatingLabel from 'react-bootstrap/esm/FloatingLabel'
+import Alert from 'react-bootstrap/Alert'
+
 const Users = () => {
     let counter = 0;
     let navigate = useNavigate();
@@ -46,6 +50,11 @@ const Users = () => {
     const handleAddUser = () => {
         console.log("Add user");
         enableForm();
+        setFormTitle("Add User");
+        setFormSubmit("Add");
+        setEmail("");
+        setPassword1("");
+        setPassword2("");
     }
 
     const handleEditUser = (email) => {
@@ -97,6 +106,29 @@ const Users = () => {
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    const [validated, setValidated] = useState(false);
+    const [InvalidInput, setInvalidInput] = useState("");
+
+    const [FormTitle, setFormTitle] = useState("");
+    const [FormSubmit, setFormSubmit] = useState("");
+    const [email, setEmail] = useState("");
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    const [errorMessage] = useState({
+        email: "This field cannot be empty!",
+        password: "This field cannot be empty!",
+    })
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        console.log("in handleSubmit");
+    }
 
     return (
         <div>
@@ -185,12 +217,86 @@ const Users = () => {
                 </div>
                 <div className={formEnabled.form}>
                     <div className="card shadow m-5 uForm">
+                        
                         <CloseButton onClick={disableForm}/>
+                        <Form
+                            noValidate 
+                            className="mt-5 mx-5" 
+                            validated={validated} 
+                            onSubmit={handleSubmit}>
+
+                            <h1 className="display-4 text-center mb-4">{FormTitle}</h1>
+
+                            {
+                            InvalidInput.length > 0 ? 
+                            <Alert variant="danger">
+                                {InvalidInput}
+                            </Alert> :
+                            <></>
+                            }
+
+                            <Form.Group className="mb-4" controlId="floatingEmail">
+                                <FloatingLabel controlId="floatingEmail" label="Email address" className="mb-3" >
+                                    <Form.Control 
+                                        required
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        autoComplete='off'
+                                    />
+
+                                    <Form.Control.Feedback type="invalid">
+                                        {errorMessage.email}
+                                    </Form.Control.Feedback>
+                                </FloatingLabel>
+                            </Form.Group>
+
+                            <Form.Group className="mb-4" controlId="floatingPassword1">
+                                <FloatingLabel controlId="floatingPassword1" label="Password" className="mb-3" >
+                                    <Form.Control 
+                                        required 
+                                        type="password" 
+                                        value={password1} 
+                                        onChange={(e) => setPassword1(e.target.value)}
+                                        autoComplete='off'
+                                    />
+
+                                    <Form.Control.Feedback type="invalid">
+                                        {errorMessage.password}
+                                    </Form.Control.Feedback>
+                                </FloatingLabel>
+                            </Form.Group>
+
+                            <Form.Group className="mb-4" controlId="floatingPassword2">
+                                <FloatingLabel controlId="floatingPassword2" label="Confirm Password" className="mb-3" >
+                                    <Form.Control 
+                                        required 
+                                        type="password" 
+                                        value={password2} 
+                                        onChange={(e) => setPassword2(e.target.value)}
+                                        autoComplete='off'
+                                    />
+
+                                    <Form.Control.Feedback type="invalid">
+                                        {errorMessage.password}
+                                    </Form.Control.Feedback>
+                                </FloatingLabel>
+                            </Form.Group>
+
+                            <div className="d-flex justify-content-center mt-5 mb-4">
+                                <Button 
+                                    type="submit" 
+                                    className="btn btn-light py-2 px-5 my-1 shadow-sm border submitButton">
+                                    {FormSubmit}
+                                </Button>
+                            </div>
+                        </Form>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+
 
 export default Users
