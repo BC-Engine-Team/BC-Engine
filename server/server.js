b2c4a74fdffac7e4b8c4e0edb5213a14bc1e2fea
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-23 15:50:33 -0500

    Added route for app.js from index.js

--- server/server.js
+++ server/server.js
@@ -9,3 +9,10 @@ const app = makeApp(databases);
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+app.get("*", (req, res) => {
+    let url = path.join(__dirname, '../client/build', 'index.html');
+    if (!url.startsWith('/app/')) // we're on local windows
+    url = url.substring(1);
+   res.sendFile(url);
+ });
