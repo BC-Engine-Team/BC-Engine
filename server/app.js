b2459de4ec38655c1283f4c8dc6eafa31c6a2766
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-23 16:42:25 -0500

    Added link to website to fix cross-origin issues

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ module.exports = (database) => {
   
 
   app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000, https://bc-engine.herokuapp.com');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, POST, GET, OPTIONS, HEAD');
     res.header(
