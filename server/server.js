c4171a8eae54e99e62a7a1893a3b99c9861e9645
Adam Mihajlovic <mihajlovicadam@gmail.com>
2022-01-05 16:10:30 -0500

    added default route

--- server/server.js
+++ server/server.js
@@ -1,22 +1,9 @@
 const databases = require('./data_access_layer/databases');
 const makeApp = require('./app');
-const express = require('express');
-const path = require('path');
 const PORT = process.env.PORT || 3001;
-const publicPath = path.join(__dirname, '..', 'public');
-
 
 const app = makeApp(databases);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
-app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/')) // we're on local windows
-    url = url.substring(1);
-   res.sendFile(url);
- });
-
- app.use(express.static(publicPath));
