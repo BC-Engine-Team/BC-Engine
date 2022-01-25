5c3246c0d953e672585e949460076e12c44687aa
cjayneb <70963776+cjayneb@users.noreply.github.com>
2022-01-25 18:34:12 -0500

    Made front chart report table work

--- client/src/pages/Reports.js
+++ client/src/pages/Reports.js
@@ -9,6 +9,8 @@ import Button from 'react-bootstrap/Button'
 
 import UnderConstruction from '../components/UnderConstruction'
 import NavB from '../components/NavB'
+import DeleteButton from '../components/DeleteButton'
+import EditButton from '../components/EditButton'
 
 const Reports = () => {
     const { t } = useTranslation();
@@ -72,7 +74,58 @@ const Reports = () => {
     return (
         <div>
             <NavB />
-            <UnderConstruction pageName="Reports" />
+            <div className='justify-content-center mainContainer'>
+                <div>
+                    <div className='card shadow'>
+                        <Table responsive="xl" hover>
+                            <thead className='bg-light'>
+                                <tr key="0">
+                                    <th>{t('reports.chartReports.Name')}</th>
+                                    <th>{t('reports.chartReports.Employee')}</th>
+                                    <th>{t('reports.chartReports.ClientType')}</th>
+                                    <th>{t('reports.chartReports.Country')}</th>
+                                    <th>{t('reports.chartReports.AgeOfAccount')}</th>
+                                    <th>{t('reports.chartReports.AccountType')}</th>
+                                    <th>{t('reports.chartReports.StartDate')}</th>
+                                    <th>{t('reports.chartReports.EndDate')}</th>
+                                    <th>
+                                        <div className="d-flex justify-content-center">
+                                            <Button
+                                                className="btn py-0 shadow-sm border">
+                                                {t('user.table.AddButton')}
+                                            </Button>
+                                        </div>
+                                    </th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {chartReports.map(r => {
+                                    return (
+                                        <tr key={r.chartReportId}>
+                                            <td>{r.name}</td>
+                                            <td>{r.employee1}{r.employee2 === null ? "" : ", " + r.employee2}</td>
+                                            <td>{r.clientType}</td>
+                                            <td>{r.country}</td>
+                                            <td>{r.ageOfAccount}</td>
+                                            <td>{r.accountType}</td>
+                                            <td>{r.startDate.toString()}</td>
+                                            <td>{r.endDate.toString()}</td>
+                                            <td className="py-1">
+                                                <div className="d-flex justify-content-center">
+                                                    <EditButton />
+
+                                                    <DeleteButton />
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
